42c8d6111e8b51bf20ba882b8a2b014ea888c274
shovz <shovz12@gmail.com>
2022-07-09 22:53:19 +0300

    game done no animation yet

--- Birds-on-a-wire/Birds-on-a-wire.js
+++ Birds-on-a-wire/Birds-on-a-wire.js
@@ -4,8 +4,16 @@ let birdBox = document.getElementById("answer__zone--selectBird");
 let resetButton = document.getElementById(`resetButton`);
 let question = document.getElementById("question__zone--Q");
 let startGame_button = document.querySelector("#start__Button");
+let restartGame_button = document.querySelector("#gameEnds_restart");
+let home_button = document.querySelector("#gameEnds_home");
 let lives = document.getElementById("question__zone--tries");
+let gameEnds = document.getElementsByClassName("end__Box");
+let gameStrats = document.getElementsByClassName("start__Box");
+let did_I_get_it = document.getElementById("question__zone--done");
+let fraction = document.createElement("div");
 let Bird_needed;
+
+
 // document.body.addEventListener("drop",()=>{
 //     let dragging_bird = document.querySelector(`.dragging_bird`);
 //     console.log(dragging_bird);
@@ -13,10 +21,39 @@ let Bird_needed;
 //     createNewBird();
 // })
 
-// const gameLoading=()=>{
 
-    // document.body.removeChild(startGame_button.parentElement);
-    // birdBox.setAttribute(`style`,`display:grid`)
+const homePage = () =>{
+    window.location.href="../Main-Page/home.html";
+}
+
+const createLives=()=>{
+    for(let i=0;i<3;i++){
+        let lives_image = document.createElement("img");
+        lives_image.setAttribute(`src`,"..//Pictures/Birds-On-a-Wire/Birds-Lives.png");
+        lives.append(lives_image);
+    }
+}
+
+const resetGame=()=>{
+    for (let i=0;i<ropes.length;i++){
+        while(ropes[i].firstChild)
+        {
+            ropes[i].removeChild(ropes[i].firstChild);
+        }
+    }
+    if (gameEnds[0].style.display==`flex` && lives.childElementCount==0)
+    { 
+            gameEnds[0].setAttribute(`style`,`display:none`);
+            createLives();
+     }
+}
+
+
+
+const gameLoading=()=>{
+    gameStrats[0].remove();
+    createLives();
+    resetButton.setAttribute("style",`display: block;`)
     const draggingOn = (event)=>{
             event.target.classList.add("dragging_bird");
     }
@@ -40,7 +77,7 @@ let Bird_needed;
                 case `purple`:
                     birdBox.prepend(newBird);
                 break;
-                case `yellow`:
+                case `Orange`:
                     birdBox.insertBefore(newBird,birdBox.lastElementChild)
                 break;
                 case `red`:                
@@ -53,7 +90,7 @@ let Bird_needed;
         
     }
     const birdsOnRope__style=(event)=>{
-            let posLeft =  12.5;
+            let posLeft =  10.5;
             
             for(let i=0;i<event.target.childNodes.length;i++)
             {
@@ -109,40 +146,37 @@ let Bird_needed;
             
     }
 
-   const resetGame=()=>{
 
-        for (let i=0;i<ropes.length;i++){
-            while(ropes[i].firstChild)
-            {
-                ropes[i].removeChild(ropes[i].firstChild);
-            }
-        }
-    }
 
     resetButton.addEventListener("click",resetGame);
 
     const createFraction=()=>{
         let numtop = Math.floor(Math.random()*10+1);
         let numbottom = Math.floor(Math.random()*10+1);
-        while(numtop>numbottom){
+        while(numtop>=numbottom){
             numtop = Math.floor(Math.random()*10+1);
             numbottom = Math.floor(Math.random()*10+1);
         }
         return `${numtop}/${numbottom}`
     }
 
-    // const question_paragraph = ()=>{
+    const question_paragraph = ()=>{
         let Bird_index=  Math.floor(Math.random()*3);
-        let fraction = document.createElement("div");
         let fraction_text = document.createTextNode(`${createFraction()}`);
-        fraction.append(fraction_text);
         Bird_needed = birds[Bird_index].classList[1];
-
+        
+        if(question.childNodes.length==0){
+            fraction.append(fraction_text);
+        } else{
+            question.removeChild(question.firstChild);
+            fraction.textContent="";
+            fraction.append(fraction_text);
+        }
         let question_Text = document.createTextNode(`
         Put ${fraction.textContent} ${Bird_needed} Birds on the Wires
         `)
         question.append(question_Text);
-    // }
+    }
 
     const didIGetIt =()=>{
         let sumOf_Birds_total=0;
@@ -159,29 +193,38 @@ let Bird_needed;
                 }
             }
         }
-        // console.log(sumOf_Birds_needed,sumOf_Birds_total);
-        // console.log(asnwer[0],asnwer[1]);
+        console.log(sumOf_Birds_needed,sumOf_Birds_total);
+        console.log(asnwer[0],asnwer[1]);
         if(sumOf_Birds_needed==asnwer[0] && sumOf_Birds_total==asnwer[1])
         {
-            console.log("yesssssssssssssss");
-        }
-
-        else{
-            console.log("nooo");
+            
+            alert("yesssssssssssssss");
             resetGame();
+            question_paragraph();
+        }
+        else{
+            alert("nooo");
             lives.removeChild(lives.lastElementChild);
             if(lives.childElementCount==0){
-                let gameEnds = document.getElementsByClassName("end__Box");
                 
-                gameEnds.classList.add("gameEnds");
+                gameEnds[0].setAttribute(`style`,`display:flex`);
+                
             }
         }
+
     
         // console.log(sumOf_Birds_total,sumOf_Birds_needed,sumOf_Birds_general);
     }
  
+    question_paragraph();
+    resetButton.addEventListener("click",question_paragraph);
+    did_I_get_it.addEventListener("click",didIGetIt);
 
+}
+startGame_button.addEventListener("click",gameLoading);
+restartGame_button.addEventListener("click",resetGame);
 
-// }
-// startGame_button.addEventListener("click",gameLoading);
+home_button.addEventListener("click",homePage);
+
 // console.log(draggingBird);
+
