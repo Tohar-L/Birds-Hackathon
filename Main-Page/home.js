2c082d1ac976b5625e8dd8278151c60f507a431b
shovz <shovz12@gmail.com>
2022-07-10 17:32:12 +0300

    bears done flotaing fish not

--- Main-Page/home.js
+++ Main-Page/home.js
@@ -1,6 +1,13 @@
 let Birds_on_a_wire = document.getElementById(`Birds_on_a_wire`);
-
+let Bears_Eating = document.getElementById(`Bears_Eating`);
 
 Birds_on_a_wire.addEventListener(`click`,()=>{
     window.location.href="../Birds-on-a-wire/Birds-on-a-wire.html";
+})
+
+
+
+
+Bears_Eating.addEventListener(`click`,()=>{
+    window.location.href="../Bears-Eating/Bears-Eating.html";
 })
